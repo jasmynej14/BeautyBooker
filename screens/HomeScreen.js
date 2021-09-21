1a31cf7e9ebc184f636bd7654cd7dc9825a1490e
jasmynej14 <jasmynej14@brandeis.edu>
2021-09-21 12:04:54 -0400

    attempt to add fonts

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import mainStyles from '../styles/MainStyles';
+
+export default function HomeScreen(){
+    
+        return(
+            <View style={mainStyles.container}>
+                <Text>Beauty Booker</Text>
+            </View>
+        )
+    
+    
+}
---
+++ styles/MainStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
+
+
+const mainStyles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"white"
+    }
+})
+
+export default mainStyles;
